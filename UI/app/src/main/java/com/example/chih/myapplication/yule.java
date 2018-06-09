a56761d573a7f0f2e3e807022a4752e8ca4f7a69
張易誠 <g22574722@gmail.com>
2018-06-09 23:48:09 +0800

    yule育樂

---
+++ UI/app/src/main/java/com/example/chih/myapplication/yule.java
@@ -0,0 +1,13 @@
+package com.example.chih.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class yule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yule);
+    }
+}
