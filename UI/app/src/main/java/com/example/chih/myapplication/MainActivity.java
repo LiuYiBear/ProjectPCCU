3590124dc58511aa1879e448431ed1cec0b31d09
LiuBear <a22313283@yahoo.com.tw>
2018-11-05 20:40:53 +0800

    完成行

--- UI/app/src/main/java/com/example/chih/myapplication/MainActivity.java
+++ UI/app/src/main/java/com/example/chih/myapplication/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity  {
                 @Override
                 public void onClick(View v) {
                     Intent intent=new Intent();
-                    intent.setClass(MainActivity.this,yule.class);
+                    intent.setClass(MainActivity.this,playfun.class);
                     startActivity(intent);
                 }
             });
